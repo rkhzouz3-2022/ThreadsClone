7bdfad299337c5f654c593e32c6d5cb40bc8f67b
Robert <robkhz@umich.edu>
2023-12-30 22:29:04 -0500

    Thread Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import User from "@/lib/models/user.model";
 import { UserButton, currentUser } from "@clerk/nextjs";
@@ -20,7 +21,7 @@ export default async function Home() {
               <ThreadCard
                 key={post._id}
                 id={post._id}
-                currentUserId={user?.id}
+                currentUserId={user?.id || ""}
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
